b251a9650f13e10b1041530adb2d92f04e0bda25
Bulat Kh <bulathisameev@MacBook-Pro-Bulat.local>
2021-09-18 01:01:52 +0300

    Initial commit

---
+++ rowReverse.php
@@ -0,0 +1,14 @@
+<?php
+function rowReverse($row){
+    $row = $row[0];
+    $isUpper = preg_match('~^\p{Lu}~u', $row);
+    $r = '';
+    for ($i = mb_strlen($row); $i>=0; $i--) {
+        $r .= mb_substr($row, $i, 1);
+    }
+    return $isUpper ? mb_convert_case($r, MB_CASE_TITLE_SIMPLE ) : $r;
+}
+
+$proposal = 'Привет! Давно не виделись.';
+$reverse = preg_replace_callback('~[А-Яа-яЁё]+~u', 'rowReverse', $proposal);
+echo $reverse;
